305391abd46cbd30f1234671a65048cd15fb60a9
Mateusz Uzdowski <mateusz>
2021-06-29 14:10:37 +1200

    Pretend dev/build error

--- app/src/Page.php
+++ app/src/Page.php
@@ -4,5 +4,9 @@ use CWP\CWP\PageTypes\BasePage;
 
 class Page extends BasePage
 {
-    
+    public function requireDefaultRecords()
+    {
+        parent::requireDefaultRecords();
+        trigger_error("Pretend error", E_USER_ERROR);
+    }
 }
